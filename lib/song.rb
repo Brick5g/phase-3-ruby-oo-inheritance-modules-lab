34a2c63964242f16725d6e4bbf0f5089aa4d8395
Julian Francis <jgfrancis3@gmail.com>
2026-07-13 11:44:17 -0400

    Inheritance modules again

--- lib/song.rb
+++ lib/song.rb
@@ -1,31 +1,16 @@
 require "pry"
 
 class Song
+  extend Memorable::ClassMethods
+  extend Findable
+  include Memorable::InstanceMethods
+  include Paramable
+
   attr_accessor :name, :artist
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect { |a| a.name == name }
-  end
-
   def self.all
     @@songs
-  end
-
-  def self.reset_all
-    all.clear
-  end
-
-  def self.count
-    all.count
-  end
-
-  def to_param
-    name.downcase.gsub(" ", "-")
   end
 end
